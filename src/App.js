cec2b7ffd4f82bf064266c4bee3b761f0034fe48
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-26 09:56:54 -0700

    git users grid display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
+// import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar title='Dev Finder' icon='fab fa-github' />
+        {/* <UserItem /> */}
+        <Users />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App; 
