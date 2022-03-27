726e8edd5e42d531b90cbfe80bb5652fe04cf2b5
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-27 06:50:00 -0700

    api call for single user

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import './App.css';
+import Search from './components/users/Search';
 
 class App extends Component {
   state = {
@@ -10,11 +11,19 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
-    this.setState({loading: true})
-    const res = await axios.get('https://api.github.com/users')
+  // async componentDidMount() {
+  //   this.setState({loading: true})
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECERT}`);
     
-    this.setState({users: res.data, loading: false})
+  //   this.setState({users: res.data, loading: false})
+  // }
+  
+  // search github users value
+  searchUsers = async (text) => {
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECERT}`);
+    
+    this.setState({users: res.data.items, loading: false})
   }
 
   render() {
@@ -22,7 +31,8 @@ class App extends Component {
       <div className="App">
         <Navbar title='Dev Finder' icon='fab fa-github' />
         <div className="container">
-        <Users loading={this.state.loading} users={this.state.users} />
+          <Search searchUsers={this.searchUsers} />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
